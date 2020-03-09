f867f273f642fa75fe8430a231346efe804d65de
pedrogouveia69 <58569642+pedrogouveia69@users.noreply.github.com>
2020-03-09 21:07:26 +0000

    Create ficha1.js

---
+++ ficha1/ficha1.js
@@ -0,0 +1,13 @@
+var x= 15;
+var y= 15;
+
+function calcMedia(notaPratica, notaTeorica) {
+    media=(notaPratica+notaTeorica)/2;
+    if (media >=9,5) {
+        console.log("Aprovado")
+    }else{
+        console.log("Reprovado")
+    }  
+}
+calcMedia(x,y)
+
